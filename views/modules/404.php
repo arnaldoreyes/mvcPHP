79a966d2744653b40ebd01f8d9e198f9a80da154
Arnaldo Reyes <arnaldoreyes13@gmail.com>
2023-01-24 13:05:16 -0400

    Segundo commit

---
+++ views/modules/404.php
@@ -0,0 +1,1 @@
+La Pagina No Existe
--- views/template.php
+++ views/template.php
@@ -35,29 +35,27 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Starter Page</h1>
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Starter Page</li>
-                            </ol>
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </div>
-            <!-- /.content-header -->
-
+            
             <!-- Main content -->
             <div class="content">
                 <div class="container-fluid">
+                    <?php
+
+                        if (isset($_GET['ruta'])) {
+                            $ruta = $_GET['ruta'];
+
+                            if ($ruta == 'users' ||
+                                $ruta == 'products' ||
+                                $ruta == 'categories'
+                                ) {
+                                include "/laragon/www/mvcPHP/views/modules/{$ruta}.php";
+                            }else{
+                                include "/laragon/www/mvcPHP/views/modules/404.php";
+                            }
+                        }
+
+                    ?>
                     
-                    <!-- /.row -->
                 </div><!-- /.container-fluid -->
             </div>
             <!-- /.content -->
